cb53983a470d1d55a21c93ac79141ba85542c500
Nauriz <tasbula03@mail.ru>
2023-05-14 12:42:01 +0600

    added comments

--- src/Student.java
+++ src/Student.java
@@ -6,11 +6,15 @@ public class Student {
         this.name = name;
         this.age = age;
     }
-
+    /*
+    method for getting the name of the student
+    */
     public String getName() {
         return name;
     }
-
+    /*
+    method for getting the age of the student
+    */
     public int getAge() {
         return age;
     }
