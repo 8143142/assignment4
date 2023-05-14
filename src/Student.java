83e112d612a1a150cd17db639bc6800a2418e6ef
Nauriz <tasbula03@mail.ru>
2023-05-14 12:39:42 +0600

    created constructor Student

--- src/Student.java
+++ src/Student.java
@@ -6,12 +6,4 @@ public class Student {
         this.name = name;
         this.age = age;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
 }
